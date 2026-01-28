c5f3cb63883dd6b3ab5b60bc34d79e008a246fa3
ashish3024 <kumarashish70800@gmail.com>
2026-01-28 15:30:43 +0530

    Replace bcrypt with bcryptjs for Render

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException, ConflictException, BadRequestException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { RegisterDto, LoginDto } from './dto/auth.dto';
 
 @Injectable()
